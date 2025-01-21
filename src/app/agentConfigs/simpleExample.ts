e42006dceb5bb3b992a393bfa2b3d317e7273ca8
mjan112a <myersjd@gmail.com>
2025-01-21 15:52:02 -0500

    change1

--- src/app/agentConfigs/simpleExample.ts
+++ src/app/agentConfigs/simpleExample.ts
@@ -2,24 +2,24 @@ import { AgentConfig } from "@/app/types";
 import { injectTransferTools } from "./utils";
 
 // Define agents
-const haiku: AgentConfig = {
-  name: "haiku",
-  publicDescription: "Agent that writes haikus.", // Context for the agent_transfer tool
+const brainstorm: AgentConfig = {
+  name: "Brainstorm",
+  publicDescription: "Agent that helps brainstorm about 10X.", // Context for the agent_transfer tool
   instructions:
-    "Ask the user for a topic, then reply with a haiku about that topic.",
+    "Ask the user for a topic, then reply brainstorms about that topic.",
   tools: [],
 };
 
 const greeter: AgentConfig = {
-  name: "greeter",
+  name: "Greeter",
   publicDescription: "Agent that greets the user.",
   instructions:
-    "Please greet the user and ask them if they'd like a Haiku. If yes, transfer them to the 'haiku' agent.",
+    "Please greet the user and ask them if they'd like to brainstorm. If yes, transfer them to the 'haiku' agent.",
   tools: [],
-  downstreamAgents: [haiku],
+  downstreamAgents: [brainstorm],
 };
 
 // add the transfer tool to point to downstreamAgents
-const agents = injectTransferTools([greeter, haiku]);
+const agents = injectTransferTools([greeter, brainstorm]);
 
 export default agents;
